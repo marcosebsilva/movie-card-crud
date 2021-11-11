aefd412aefbf71c6680c6139ce928a437582617f
Marcos Estevao <dusdoom@gmail.com>
2021-11-10 21:07:46 -0300

    restoring old Router structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
+import './App.css';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route path="/:any" component={ NotFound } />
       </Switch>
