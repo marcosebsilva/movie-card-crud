db793bd3f78d4ff6d1ea7acf3cefeffed06ac1a9
Marcos Estevao <dusdoom@gmail.com>
2021-11-10 21:07:19 -0300

    changed NotFound page structure

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import funnyGuy from '../images/funny_guy.png';
 
 class NotFound extends Component {
   render() {
-    return <div data-testid="404-error">Página não encontrada</div>;
+    return (
+      <>
+        <div data-testid="404-error">
+          Página não encontrada.
+        </div>
+        <img
+          src={ funnyGuy }
+          alt="Hulk dancing"
+        />
+      </>
+    );
   }
 }
 
