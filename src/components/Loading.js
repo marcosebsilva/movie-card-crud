d711453472a3968dddbd55348bdd372be3af2b57
Marcos Estevao <dusdoom@gmail.com>
2021-11-11 17:27:13 -0300

    added details page + loader

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Footer } from '.';
 
 class Loading extends Component {
   render() {
     return (
-      <div>
-        <p>Carregando...</p>
-      </div>
+      <>
+        <div className="loading">
+          <div className="loading__circle" />
+          <p className="loading__text">Carregando...</p>
+        </div>
+        <Footer />
+      </>
     );
   }
 }
