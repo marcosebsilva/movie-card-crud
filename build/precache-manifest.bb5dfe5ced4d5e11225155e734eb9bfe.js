4531208fb2bb0aa4f771350c12804bd940be7b22
Marcos Estevao <dusdoom@gmail.com>
2021-11-12 05:08:10 -0300

    added build folder

---
+++ build/precache-manifest.bb5dfe5ced4d5e11225155e734eb9bfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d49dec9dffb49ec69db5577f1fad07",
+    "url": "/movie-card-crud/index.html"
+  },
+  {
+    "revision": "1e3dc9e5222e622af192",
+    "url": "/movie-card-crud/static/css/main.41b1bfbd.chunk.css"
+  },
+  {
+    "revision": "24b04df69c2928117338",
+    "url": "/movie-card-crud/static/js/2.4fed7fd7.chunk.js"
+  },
+  {
+    "revision": "1e3dc9e5222e622af192",
+    "url": "/movie-card-crud/static/js/main.1bbc30a5.chunk.js"
+  },
+  {
+    "revision": "7582370dd4f02f9c63fe",
+    "url": "/movie-card-crud/static/js/runtime-main.bdb2e81f.js"
+  },
+  {
+    "revision": "93c7d9a219b1c8d012944ff99cc4c9ce",
+    "url": "/movie-card-crud/static/media/funny_guy.93c7d9a2.png"
+  }
+]);
